77f80b39d1215d18a47151072cb8dfb58858066a
Matt Neil <mneil@wi.rr.com>
2015-03-02 06:05:05 -0600

    everything basically works, although not very robust i.e editing the shortocde will cause extra option rows, nothing really styled

--- student-choice.php
+++ student-choice.php
@@ -11,6 +11,356 @@ License:        GNLv2 (or higher)
 // This software from 2014, put together quickly.
 // Here just as reference.
 
+/**
+ * Creates needed tables on plugin activation
+ * TODO: figure out how to make foriegn keys
+ */
+function sc_activation() {
+    global $wpdb;
+
+    $table_options = $wpdb->prefix.'sc_options';
+    $table_participants = $wpdb->prefix.'sc_participants';
+    $table_questions = $wpdb->prefix.'sc_questions';
+    $table_poll = $wpdb->prefix.'sc_poll';
+    $table_votes = $wpdb->prefix.'sc_votes';
+    $charset_collate = $wpdb->get_charset_collate();
+
+    $sql1 = "CREATE TABLE $table_options (
+            id int(11) NOT NULL AUTO_INCREMENT,
+            question_id int(11) NOT NULL,
+            option_text text,
+            PRIMARY KEY (id),
+            KEY question_id (question_id)
+    ) $charset_collate;";
+
+    $sql2 = "CREATE TABLE $table_participants (
+            id int(11) NOT NULL AUTO_INCREMENT,
+            email text NOT NULL,
+            poll_name varchar(250) NOT NULL,
+            PRIMARY KEY (id),
+            KEY poll_name (poll_name)
+    ) $charset_collate;";
+
+    $sql3 = "CREATE TABLE $table_questions (
+            id int(11) NOT NULL AUTO_INCREMENT,
+            poll_name varchar(250) NOT NULL,
+            question_name varchar(250) NOT NULL,
+            PRIMARY KEY (id),
+            KEY poll_name (poll_name)
+    ) $charset_collate;";
+
+    $sql4 = "CREATE TABLE $table_poll (
+            poll_name varchar(250) NOT NULL,
+            PRIMARY KEY (poll_name)
+    ) $charset_collate;";
+    
+    //TODO: add question id to this table
+    $sql5 = "CREATE TABLE $table_votes (
+            id int(11) NOT NULL AUTO_INCREMENT,
+            participant_id int(11) NOT NULL,
+            option_id int(11) NOT NULL,
+            PRIMARY KEY (id),
+            KEY participant_id (participant_id),
+            KEY option_id (option_id)
+    ) $charset_collate;";
+
+    
+    require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+    dbDelta($sql1);
+    dbDelta($sql2);
+    dbDelta($sql3);
+    dbDelta($sql4);
+    dbDelta($sql5);
+}
+register_activation_hook( __FILE__, 'sc_activation');
+
+class StudentChoicePoll
+{
+    public $display_form;
+    public $valid_form;
+    public $poll_name;
+    public $poll_questions;
+    public function __construct()
+    {
+        $this->display_form = true;
+        $this->valid_form = true;
+        add_action('init', array($this, 'init'));
+        add_shortcode('poll', array($this, 'poll_shortcode'));
+        add_shortcode('poll-options', array($this, 'poll_options_shortcode'));
+    }
+
+    public function init()
+    {
+        if (!empty($_POST['sc_poll_form_nonce']))
+        {
+            if (!wp_verify_nonce($_POST['sc_poll_form_nonce'], 'sc_poll_form'))
+            {
+                die('Not authorized');
+            }
+            else
+            {
+                if (empty($_POST['sc_poll_email']) || !$this->valid_wisc($_POST['sc_poll_email']))
+                {
+                    $this->valid_form = false;
+                }
+                else
+                {
+                    //valid wisc email has been entered, lets start processing the votes.
+                    $this->display_form = false;
+                    $this->poll_name = $_POST['poll_name'];
+                    $email = $_POST['sc_poll_email'];
+                    $participant = $this->find_participant($email);
+                    $participant_id;
+                    if (empty($participant))
+                    {
+                        $participant_id = $this->create_participant($email);
+                    }
+                    else
+                    {
+                        $participant_id = $participant[0]->id;
+                    }
+                    $this->poll_questions = $this->get_questions($this->poll_name);
+                    foreach ($this->poll_questions as $question)
+                    {
+                        if (isset($_POST['question_'.$question->id]))
+                        {
+                            $opt_id = $_POST['question_'.$question->id];
+                            if (!$this->option_voted_on($participant_id, $opt_id))
+                            {
+                                $this->add_vote($participant_id, $opt_id);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    function poll_shortcode($atts, $content = '')
+    {
+        global $wpdb;
+
+        $att = shortcode_atts(array(
+            'name' => 'default-poll'
+        ), $atts);
+        $this->poll_name = $att['name'];
+        $this->poll_questions = $this->get_questions($this->poll_name);
+        if ($this->display_form)
+        {
+            //Form has not been submitted or had errors, lets display the form.
+            $ret = '<form action="" method="POST">';
+            $ret .= '<input type="text" name="poll_name" value="'.$this->poll_name.'" hidden>';
+            $ret .= do_shortcode($content);
+            $ret .= '<div class="email">';
+            $ret .= '<label for="sc_poll_email" class="email-input-label">Please insert your valid @wisc.edu email.</label>';
+            if (!$this->valid_form)
+            {
+                $ret .= '<p class="email-err">Please enter a valid @wisc.edu email.</p>';
+            }
+            $ret .= '<input name="sc_poll_email" id="sc_poll_email" class="email-input" type="text" placeholder="Email">';
+            $ret .= '</div>';
+            $ret .= wp_nonce_field('sc_poll_form', 'sc_poll_form_nonce');
+            $ret .= '<input type="submit" value="Submit">';
+            $ret .= '</form>';
+            return $ret;
+        }
+        else
+        {
+            //Form has been submitted and we have returned here from form action.
+            $ret = '<div>Thanks for voting</div>';
+            return $ret;
+        }
+    }
+
+    function poll_options_shortcode($atts, $content = '')
+    {
+        global $wpdb;
+
+        $att = shortcode_atts(array(
+            'question' => 'default-question',
+            'options' => 'default-options'
+        ), $atts);
+        $question_slug = strtolower($att['question']);
+        $question_slug = str_replace(" ", "-", $question_slug);
+        $att['options'] = explode(',', $att['options']);
+        $q = $this->has_question($question_slug);
+        if (!$q)
+        {
+            $inserted = $wpdb->insert(
+                $wpdb->prefix.'sc_questions',
+                array(
+                    'question_name' => $question_slug,
+                    'poll_name' => $this->poll_name
+                ),
+                array(
+                    '%s',
+                    '%s'
+                )
+            );
+            if ($inserted)
+            {
+                $qid = $wpdb->insert_id;
+                foreach ($att['options'] as $key => $option)
+                {
+                    $inserted = $wpdb->insert(
+                        $wpdb->prefix.'sc_options',
+                        array(
+                            'question_id' => $qid,
+                            'option_text' => $option
+                        ),
+                        array(
+                            '%d',
+                            '%s'
+                        )
+                    );
+                }
+            }
+
+            $this->poll_questions = $this->get_questions($this->poll_name);
+        }
+        else
+        {
+            //TODO: check for new options if the shortcode had been edited
+            //TODO: also would be nice to be able to remove options no longer
+            //      present
+        }
+        $q = $this->has_question($question_slug);
+        $ret = '<div class="sc_poll_question">';
+        foreach ($q->options as $key => $option)
+        {
+            $checked = '';
+            if (isset($_POST['question_'.$q->id])) {
+                if ((int)$option->id === (int) $_POST['question_'.$q->id])
+                {
+                    $checked = ' checked';
+                }
+            }
+            $ret .= '<input type="radio" id="sc_poll_'.$q->id.'_'.$option->id.'" name="question_'.$q->id.'" value="'.$option->id.'"'.$checked.'>';
+            $ret .= '<label for="sc_poll_'.$q->id.'_'.$option->id.'">'.$option->option_text.'</label><br />';
+        }
+        $ret .= '</div>';
+        return $ret;
+    }
+
+    function get_questions($poll_name)
+    {
+        global $wpdb;
+        $sql = $wpdb->prepare("SELECT * FROM {$wpdb->prefix}sc_questions
+                                WHERE poll_name = %s",
+                                $poll_name);
+        $result = $wpdb->get_results($sql);
+
+        $questions = array();
+
+        //Could probably be more efficient here with a JOIN but I'm an SQL noob
+        foreach ($result as $key => & $val)
+        {
+            $sql = $wpdb->prepare("SELECT * FROM {$wpdb->prefix}sc_options
+                WHERE question_id = %d",
+                $val->id
+            );
+            $opts = $wpdb->get_results($sql);
+            $val->options = $opts;
+        }
+
+        return $result;
+    }
+
+    function valid_wisc($email) {
+        $email = strtolower($email);
+        if (filter_var($email, FILTER_VALIDATE_EMAIL) === FALSE) {
+            return FALSE;
+        } else {
+            if (strpos($email, "wisc.edu") === FALSE) {
+                return FALSE;
+            }
+        }
+        return TRUE;
+    }
+
+    function has_question($question_slug)
+    {
+        foreach ($this->poll_questions as $question)
+        {
+            if (strcmp($question->question_name, $question_slug) === 0) 
+            {
+                return $question;
+            }
+        }
+        return false;
+    }
+
+    function find_participant($email)
+    {
+        global $wpdb;
+        $sql = $wpdb->prepare("SELECT * FROM {$wpdb->prefix}sc_participants
+            WHERE email = %s
+            AND poll_name = %s",
+            $email,
+            $this->poll_name
+        );
+        $result = $wpdb->get_results($sql);
+        return $result;
+    }
+
+    function create_participant($email)
+    {
+        global $wpdb;
+        $sql = $wpdb->insert($wpdb->prefix.'sc_participants',
+            array(
+                'email' => $email,
+                'poll_name' => $this->poll_name
+            ),
+            array(
+                '%s',
+                '%s'
+            )
+        );
+        return $wpdb->insert_id;
+    }
+
+    //TODO bit confused why we check if a participant voted on an option and not question
+    //Should really check question ID so participants cant double vote
+    function option_voted_on($participant_id, $option_id) {
+        global $wpdb;
+        $voted_stmt = $wpdb->prepare("SELECT * FROM {$wpdb->prefix}sc_votes
+            WHERE participant_id = %d
+            AND option_id = %d",
+            $participant_id,
+            $option_id
+        );
+        $result = $wpdb->get_results($sql);
+        if (empty($result)) 
+        {
+            return false;
+        } 
+        else 
+        {
+            return true;
+        }
+    }
+
+    //TODO: Add question id
+    function add_vote($participant_id, $option_id) {
+        global $wpdb;
+        if ($option_id === null) {
+            return;
+        }
+        $add_stmt = $wpdb->insert($wpdb->prefix.'sc_votes',
+            array(
+                'participant_id' => $participant_id,
+                'option_id' => $option_id
+            ),
+            array(
+                '%d',
+                '%d'
+            )
+        );
+    }
+}
+
+$poll = new StudentChoicePoll();
+/*
 function open_db($dbstr, $username, $password, $options) {
     $dbh = new PDO($dbstr, $username, $password);
     $dbh->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
@@ -83,6 +433,7 @@ function valid_wisc($email) {
     }
     return TRUE;
 }
+*/
 /*
 ?>
 
@@ -255,4 +606,5 @@ function valid_wisc($email) {
 				</article><!-- #post -->
 
 		</div><!-- #content -->
-<?php get_footer(); ?>
+<?php get_footer(); 
+*/
